2f2453d1a737a8dea8c9de33362d08f1c95665f1
Muhammed Ali CAN <tmwwdw@gmail.com>
2025-03-22 19:26:03 +0300

    feat@backend: implement tag and attachment features

--- backend/src/models/todo.ts
+++ backend/src/models/todo.ts
@@ -1,10 +1,20 @@
-import mongoose from "mongoose";
+import { Schema, model } from "mongoose";
 
-const schema = new mongoose.Schema({
+const schema = new Schema({
   title: String,
   description: String,
   priority: String,
   author: String,
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  attachments: [{ type: Schema.Types.ObjectId, ref: "File" }],
+  created_at: Date,
 });
 
-export const Todo = mongoose.model("Todo", schema);
+export const Todo = model("Todo", schema);
+
+const tagSchema = new Schema({
+  title: String,
+  author: String,
+});
+
+export const Tag = model("Tag", tagSchema);
